d739a7f9085a5be7a92c2d9f7016d22a8308c2f3
Rodrigo Landerdahl <rlanderdahl@gmail.com>
2016-02-16 16:47:00 -0200

    refactor: namespacing

--- static/js/post.js
+++ static/js/post.js
@@ -1,170 +1,177 @@
-// Função responsável por "desenhar" um estado de "pego" pro post
-function grabPostUI($post, x, y, scale, angle) {
-  var post_half_width = $post.width() / 2;
-  var post_half_height = $post.height() / 2;
-  var top = $post.offset().top;
 
-  var scale_proportion = ((scale - 1) / scale);
+(function () {
 
-  $post.css("transform",
-    "translate3d(0, 0, 0) scale(" + scale + ") " +
-    "translate(" +
-    (scale_proportion * (post_half_width - x)) + "px," +
-    (scale_proportion * (post_half_height - y + top)) + "px)");
-}
+  'use strict';
 
-/* Função responsável por movimentar o post
-baseado na posição do dedo do usuário
-*/
-function setMovablePost($posts) {
+  // Função responsável por "desenhar" um estado de "pego" pro post
+  function grabPostUI($post, x, y, scale, angle) {
+    var post_half_width = $post.width() / 2;
+    var post_half_height = $post.height() / 2;
+    var top = $post.offset().top;
 
-  var scale = 0.85;
-  var angle = 10;
+    var scale_proportion = ((scale - 1) / scale);
 
-  $posts.on("touchstart", ".post-container", function(e) {
+    $post.css("transform",
+      "translate3d(0, 0, 0) scale(" + scale + ") " +
+      "translate(" +
+      (scale_proportion * (post_half_width - x)) + "px," +
+      (scale_proportion * (post_half_height - y + top)) + "px)");
+  }
 
-    // Salva a posição xy inicial do dedo na tela
-    var initial_x = e.originalEvent.touches[0].pageX;
-    var initial_y = e.originalEvent.touches[0].pageY;
+  /* Função responsável por movimentar o post
+  baseado na posição do dedo do usuário
+  */
+  function setMovablePost($posts) {
 
-    // Cacheia elementos necessários para a mágica
-    var $post_container = $(this);
-    var $post = $post_container.find(".post");
-    var post_width = $post.width();
+    var scale = 0.85;
+    var angle = 10;
 
-    var $feature_header = $("#feature-header");
+    $posts.on("touchstart", ".post-container", function(e) {
 
-    // Espera x segundos para ver se o usuário não soltou o dedo
-    /*
-      Aqui, estou checando se o "touch-status" do post foi modificado
-      por algum outro evento, caso tenha sido (muito provavelmente para 0),
-      nada deve acontecer.
-    */
-    var timeout = setTimeout(function() {
+      // Salva a posição xy inicial do dedo na tela
+      var initial_x = e.originalEvent.touches[0].pageX;
+      var initial_y = e.originalEvent.touches[0].pageY;
 
-      // 1. Muda o estado do post para "held"
+      // Cacheia elementos necessários para a mágica
+      var $post_container = $(this);
+      var $post = $post_container.find(".post");
+      var post_width = $post.width();
+
+      var $feature_header = $("#feature-header");
+
+      // Espera x segundos para ver se o usuário não soltou o dedo
       /*
-        Estou, com isso, fazendo uma máquina de estados,
-        podendo controlar o estado do post dependendo
-        do atributo "data"
+        Aqui, estou checando se o "touch-status" do post foi modificado
+        por algum outro evento, caso tenha sido (muito provavelmente para 0),
+        nada deve acontecer.
       */
-      $post_container.data("touch-status", "held");
+      var timeout = setTimeout(function() {
 
-      // 2. Indicar que foi "pego"
-      // Muda o tamanho do post para uma escala x predefinida
+        // 1. Muda o estado do post para "held"
+        /*
+          Estou, com isso, fazendo uma máquina de estados,
+          podendo controlar o estado do post dependendo
+          do atributo "data"
+        */
+        $post_container.data("touch-status", "held");
+
+        // 2. Indicar que foi "pego"
+        // Muda o tamanho do post para uma escala x predefinida
+        /*
+          É importante notar que dividi dois transforms:
+          um para o post e outro pra seu container pai.
+          O post é responsável por girar e o container pai
+          por mover. Foi necessário fazer isso devido a problemas
+          de herança no z-index e devido à animações que necessitam
+          ocorrer simultaneamente.
+        */
+        grabPostUI($post, initial_x, initial_y, scale, angle);
+
+        // Muda a classe do post para "held"
+        /*
+          Essa é a parte responsável por deixar o post a cima
+          dos outros, além de levemente transparente
+        */
+        $post_container.addClass("held");
+
+        // 3. Remove classe de transição
+        /*
+          Deve-se remover a classe de transição
+          de animação toda vez que for começar a
+          movimentar o post, já que não queremos
+          delay no movimento 
+        */
+        $post_container.removeClass("post-transition");
+
+        /* Extras relacionados à outras funcionalidades */
+        // Barra de "ver depois"
+        /*
+          Quando o usuário segura o post, devem aparecer diversas
+          opções na tela, como a de "ver depois".
+          Ao soltar o post na barra de ver depois, o usuário
+          salva seu post para uma leitura posterior e continua
+          com a navegação.
+          O post deve sumir após isso.
+        */
+        $feature_header.slideDown();
+      }, 100);
+
+      // Muda o status do post para o id do setTimeout
       /*
-        É importante notar que dividi dois transforms:
-        um para o post e outro pra seu container pai.
-        O post é responsável por girar e o container pai
-        por mover. Foi necessário fazer isso devido a problemas
-        de herança no z-index e devido à animações que necessitam
-        ocorrer simultaneamente.
+        Fica mais fácil de cancelar o setTimeout se eu tiver
+        guardado seu id
       */
-      grabPostUI($post, initial_x, initial_y, scale, angle);
+      $post_container.data("touch-status", timeout);
 
-      // Muda a classe do post para "held"
-      /*
-        Essa é a parte responsável por deixar o post a cima
-        dos outros, além de levemente transparente
-      */
-      $post_container.addClass("held");
+      // Aqui fica a parte responsável pela movimentação do post_container
+      $post_container.on("touchmove", function(e) {
+        if ($post_container.data("touch-status") === "held") {
+          e.preventDefault();
+          $post_container.css({
+            "transform": "translate3d(0, 0, 0) translate(" +
+              (e.originalEvent.touches[0].pageX-initial_x)/scale + "px, " +
+              (e.originalEvent.touches[0].pageY-initial_y)/scale + "px) " +
+              "rotate(" + angle*(((e.originalEvent.touches[0].pageX-initial_x)/scale)/post_width) + "deg)"
+          });
 
-      // 3. Remove classe de transição
-      /*
-        Deve-se remover a classe de transição
-        de animação toda vez que for começar a
-        movimentar o post, já que não queremos
-        delay no movimento 
-      */
-      $post_container.removeClass("post-transition");
-
-      /* Extras relacionados à outras funcionalidades */
-      // Barra de "ver depois"
-      /*
-        Quando o usuário segura o post, devem aparecer diversas
-        opções na tela, como a de "ver depois".
-        Ao soltar o post na barra de ver depois, o usuário
-        salva seu post para uma leitura posterior e continua
-        com a navegação.
-        O post deve sumir após isso.
-      */
-      $feature_header.slideDown();
-    }, 100);
-
-    // Muda o status do post para o id do setTimeout
-    /*
-      Fica mais fácil de cancelar o setTimeout se eu tiver
-      guardado seu id
-    */
-    $post_container.data("touch-status", timeout);
-
-    // Aqui fica a parte responsável pela movimentação do post_container
-    $post_container.on("touchmove", function(e) {
-      if ($post_container.data("touch-status") === "held") {
-        e.preventDefault();
-        $post_container.css({
-          "transform": "translate3d(0, 0, 0) translate(" +
-            (e.originalEvent.touches[0].pageX-initial_x)/scale + "px, " +
-            (e.originalEvent.touches[0].pageY-initial_y)/scale + "px) " +
-            "rotate(" + angle*(((e.originalEvent.touches[0].pageX-initial_x)/scale)/post_width) + "deg)"
-        });
-
-        // Tratando quando o usuário deixa o post sobre "ver depois"
-        if(e.originalEvent.touches[0].clientY <= 78){
-          if($feature_header.hasClass("off-feature")){
+          // Tratando quando o usuário deixa o post sobre "ver depois"
+          if(e.originalEvent.touches[0].clientY <= 78){
+            if($feature_header.hasClass("off-feature")){
+              $feature_header
+                .removeClass("off-feature")
+                .addClass("on-feature");
+              $post_container.css("opacity", 0);
+            }
+          } else if($feature_header.hasClass("on-feature")){
             $feature_header
-              .removeClass("off-feature")
-              .addClass("on-feature");
-            $post_container.css("opacity", 0);
+              .removeClass("on-feature")
+              .addClass("off-feature");
+            $post_container.css("opacity", 1);
           }
-        } else if($feature_header.hasClass("on-feature")){
-          $feature_header
-            .removeClass("on-feature")
-            .addClass("off-feature");
-          $post_container.css("opacity", 1);
+
+        } else {
+          clearTimeout(timeout);
+          $post_container.off("touchmove");
         }
-
-      } else {
-        clearTimeout(timeout);
-        $post_container.off("touchmove");
-      }
-      return true;
+        return true;
+      });
     });
+
+    $posts.on("touchend", ".post-container", function(e) {
+
+      var $post_container = $(this);
+      var $post = $post_container.find(".post");
+      var $feature_header = $("#feature-header");
+
+      // Acaba com o timeout, caso ele ainda esteja em espera
+      clearTimeout($post_container.data("touch-status"));
+      console.log(e);
+      if(e.originalEvent.changedTouches[0].clientY <= 78){
+        $post_container.remove();
+      } else{
+        // Reseta todo o processo de animação
+        $post_container.removeData("touch-status")
+          .off("touchmove")
+          .removeClass("held")
+          .addClass("post-transition")
+          .css("transform", "translate(0,0)");
+        $post.css("transform", "scale(1) translate(0, 0)");
+      }
+
+      $feature_header
+        .removeClass("on-feature")
+        .addClass("off-feature");
+      
+      /* Extras relacionados à outras funcionalidades */
+      $feature_header.slideUp();
+    });
+  }
+
+  $(document).ready(function() {
+
+    var $posts = $(".posts-container");
+
+    setMovablePost($posts);
   });
 
-  $posts.on("touchend", ".post-container", function(e) {
-
-    var $post_container = $(this);
-    var $post = $post_container.find(".post");
-    var $feature_header = $("#feature-header");
-
-    // Acaba com o timeout, caso ele ainda esteja em espera
-    clearTimeout($post_container.data("touch-status"));
-    console.log(e);
-    if(e.originalEvent.changedTouches[0].clientY <= 78){
-      $post_container.remove();
-    } else{
-      // Reseta todo o processo de animação
-      $post_container.removeData("touch-status")
-        .off("touchmove")
-        .removeClass("held")
-        .addClass("post-transition")
-        .css("transform", "translate(0,0)");
-      $post.css("transform", "scale(1) translate(0, 0)");
-    }
-
-    $feature_header
-      .removeClass("on-feature")
-      .addClass("off-feature");
-    
-    /* Extras relacionados à outras funcionalidades */
-    $feature_header.slideUp();
-  });
-}
-
-$(document).ready(function() {
-
-  var $posts = $(".posts-container");
-
-  setMovablePost($posts);
-});
+})()
