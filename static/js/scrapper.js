81cc3f0f9ca1bc488bff7b88feac1855b644d437
Ramon <ramondiogo10.1@gmail.com>
2016-02-23 20:43:58 -0300

    fix: facebook videos

---
+++ static/js/scrapper.js
@@ -0,0 +1,22 @@
+(function($) {
+
+  $.fn.scraper = function (options){
+    this.each(function(){
+      var element = $(this);
+      var defaults = {width: element.parent().width()};
+      options = $.extend(defaults, options || {});
+      var href = element.attr('href');
+      $.ajax({
+        url: "https://scraper.luccasmaso.com/api/v1/oembed?url=" + encodeURIComponent(href) + "&width=" + options.width + "&autoplay=1"
+      }).done(function(response){
+        if(response.type == "rich" || response.type == "video"){
+          element.replaceWith(response.html);
+          if(options.done){
+            options.done.call();
+          }
+        }
+      });
+    });
+  };
+
+}(jQuery, window));
