f7a8fab69a65c21d1c341023ab96c27ffa58aa59
Ramon <ramondiogo10.1@gmail.com>
2016-02-25 14:46:01 -0300

    add: hashtag event property slug again

--- static/js/events.js
+++ static/js/events.js
@@ -2,11 +2,14 @@
   $(document).ready(function(){
 
     function page_view(slug){
-
+      amplitude.logEvent("pageview", { slug: slug });
     }
+
     var $posts_container = $(".posts-container");
 
-    amplitude.logEvent("pageview", { slug: $posts_container.data("hashtag-slug") });
-
+    // Properties
+    var hashtag_slug = $posts_container.data("hashtag-slug");
+    
+    pageview(hashtag_slug);
   });
 })();
