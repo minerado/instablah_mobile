5a689f21970518cfe34581310d21bd2d77af1fa2
Ramon <ramondiogo10.1@gmail.com>
2016-02-29 21:44:27 -0300

    add: share event

--- static/js/events.js
+++ static/js/events.js
@@ -1,15 +1,32 @@
-(function(){
-  $(document).ready(function(){
+(function() {
+    $(document).ready(function() {
+        /* Variable declaration */
+        var $posts_container = $(".posts-container");
+        var hashtag_slug = $posts_container.data("hashtag-slug");
 
-    function page_view(slug){
-      amplitude.logEvent("pageview", { slug: slug });
-    }
+        /* Function declaration */
+        function page_view(slug) {
+            amplitude.logEvent("pageview", {
+                slug: slug
+            });
+        }
 
-    var $posts_container = $(".posts-container");
+        function share(hashtag_slug, post_slug, post_type) {
+            amplitude.logEvent("share", {
+                hashtag:   hashtag_slug,
+                post:      post_slug,
+                post_type: post_type
+            });
+        }
 
-    // Properties
-    var hashtag_slug = $posts_container.data("hashtag-slug");
-    
-    page_view(hashtag_slug);
-  });
+        /* Events */
+        $posts_container.on('touchstart', '.post-share-button', function(){
+            var $post = $(this).closest('.post');
+            share(hashtag_slug, $post.data('slug'), $post.data('type'));
+        });
+
+        /* Main */
+
+        page_view(hashtag_slug);
+    });
 })();
